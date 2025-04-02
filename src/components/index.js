af689d9e937e4acf88a349700ab67a9419628689
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-04-02 10:08:58 +0500

    a

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Header } from "./Header";
+export { default as Main } from "./Main";
+export { default as Slider } from "./Slider";
+export { default as Button } from "./Button";
+export { default as Slider2 } from "./Slider2";
+export { default as MyMap } from "./MyMap";
+export { default as Input } from "./Input";
